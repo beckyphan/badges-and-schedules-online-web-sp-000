b79bc985e9f06a7deb54a57afb626ac2c901af70
Becky Phan <beckyphan@live.com>
2019-05-24 00:21:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def batch_badge_creator(speakers)
+  speakers.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(speakers,rooms)
+  list = []
+  speakers.each_with_index do |name, index|
+    list << "Hello, #{name}! You'll be assigned to room #{rooms[index]}!"
+  end
+  list
+end
+
+def printer(speakers, rooms)
+  puts batch_badge_creator(speakers)
+  puts assign_rooms(speakers, names)
+end
+  
